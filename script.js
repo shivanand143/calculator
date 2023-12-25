2596088507ba1430f14f82a88f639604d1a80cda
shivanand143 <154795271+shivanand143@users.noreply.github.com>
2023-12-25 18:02:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+  function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+  }
+
+  function calculateResult() {
+    const displayValue = document.getElementById('display').value;
+    try {
+      const result = eval(displayValue);
+      document.getElementById('display').value = result;
+    } catch (error) {
+      document.getElementById('display').value = 'Error';
+    }
+  }
+
+  function clearDisplay() {
+    document.getElementById('display').value = '';
+  }
+
